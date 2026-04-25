f812483efc8d3609237920d6a00a76d96385ca6a
sanketbarot <92026165+sanketbarot@users.noreply.github.com>
2026-04-25 14:55:38 +0530

    Update anniversary date to April 25, 2026

--- js/special.js
+++ js/special.js
@@ -6,7 +6,7 @@
 // ===== Check If Anniversary Day =====
 function checkAnniversaryDay() {
     const now = new Date();
-    const anniversaryDate = new Date('April 24, 2026');
+    const anniversaryDate = new Date('April 25, 2026');
 
     const isAnniversaryDay =
         now.getFullYear()  === anniversaryDate.getFullYear() &&
